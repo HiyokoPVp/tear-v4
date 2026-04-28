204dd725d5b061637dda5898c8932595f3a90f88
HiyokoPVp <harukikanamari@gmail.com>
2026-04-29 03:41:24 +0900

    Initialize tear mode and queue teleport script
    Set tear mode and queue teleport code for execution.

--- Mainscript.lua
+++ Mainscript.lua
@@ -1,1 +1,19 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/HiyokoPVp/DOGVAPE/refs/heads/main/init.lua'), 'init.lua')({})
+
+getgenv().tearmode = true
+
+local code = [[
+getgenv().tearmode = true
+
+if queue_on_teleport then
+    queue_on_teleport([==[
+getgenv().tearmode = true
+
+if queue_on_teleport then
+    queue_on_teleport(...)
+end
+]==])
+end
+]]
+
+queue_on_teleport(code)
